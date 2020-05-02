0144df43d0e9a620e012356507941dc96d672ebd
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-03 01:43:03 +0300

    initial commit

---
+++ InstagramCloneIOS/InstagramCloneIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramCloneIOS
+//
+//  Created by Okan Serdaroğlu on 3.05.2020.
+//  Copyright © 2020 Okan Serdaroğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
