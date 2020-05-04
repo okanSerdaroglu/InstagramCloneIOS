e520c4264f2b9c9e9f51e222cdbb2ac50202bd0f
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-04 03:00:03 +0300

    sign up user

--- InstagramCloneIOS/InstagramCloneIOS/ViewController.swift
+++ InstagramCloneIOS/InstagramCloneIOS/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -20,9 +21,33 @@ class ViewController: UIViewController {
 
 
     @IBAction func buttonSignUp(_ sender: Any) {
+         if textFieldUserName.text != "" && textFieldPassword.text != ""{
+                   Auth.auth().createUser(withEmail: textFieldUserName.text!, password: textFieldPassword.text!) { (authData, error) in
+                       
+                       if error != nil {
+                           self.makeAlert(title: "Error!", message: error?.localizedDescription ?? "Error")
+                       } else {
+                           self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                       }
+                   
+                   }
+               } else {
+                  makeAlert(title: "Error!", message: "Username/Password")
+               }
     }
+    
     @IBAction func buttonSignIn(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+       
+        
     }
+    
+    func makeAlert(title:String,message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+                   let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                   alert.addAction(okButton)
+                   self.present(alert,animated: true,completion: nil)
+    }
+    
 }
 
