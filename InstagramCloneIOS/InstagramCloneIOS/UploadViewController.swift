c6759ab449fb984dacb44d03ba03beaa36f85021
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-04 18:31:45 +0300

    upload image with uuid

--- InstagramCloneIOS/InstagramCloneIOS/UploadViewController.swift
+++ InstagramCloneIOS/InstagramCloneIOS/UploadViewController.swift
@@ -31,6 +31,10 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate & 
         
     }
     
+    func makeAlert (titleInput:String,messageInput:String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageViewSelected.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
@@ -46,15 +50,18 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate & 
         
         if let data = imageViewSelected.image?.jpegData(compressionQuality: 0.5){
             
-            let imageReference = mediaFolder.child("image.jpg")
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
             imageReference.putData(data, metadata: nil) { (metaData, error) in
                 if error != nil {
-                    print(error?.localizedDescription)
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error while uploading")
                 } else {
                     imageReference.downloadURL { (url, error) in
                         if error == nil {
                             let imageUrl = url?.absoluteString
-                            print(imageUrl)
+                           
+                            // send to database
                         }
                     }
                     
